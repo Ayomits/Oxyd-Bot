0f6682fe9738a93d341e9f9c440dd2790d66b66f
Ayomi <nikitakabo74@gmail.com>
2024-08-23 21:37:29 +0300

    fix: pagination bugs

--- src/modules/utils-module/InroleCommand.ts
+++ src/modules/utils-module/InroleCommand.ts
@@ -8,7 +8,11 @@ import {
   ComponentType,
   EmbedBuilder,
   GuildMember,
+  ModalBuilder,
   SlashCommandBuilder,
+  StringSelectMenuBuilder,
+  TextInputBuilder,
+  TextInputStyle,
   userMention,
 } from "discord.js";
 import _ from "lodash";
@@ -18,11 +22,11 @@ export class InRole extends BaseCommand {
     super({
       builder: new SlashCommandBuilder()
         .setName(`inrole`)
-        .setDescription(`Список участников имеющих определенную роль`)
+        .setDescription(`Список участников, имеющих определенную роль`)
         .addRoleOption((option) =>
           option
             .setName(`role`)
-            .setDescription(`Укажите здесь нужную Вам роль`)
+            .setDescription(`Укажите нужную роль`)
             .setRequired(true)
         )
         .addNumberOption((option) =>
@@ -40,6 +44,7 @@ export class InRole extends BaseCommand {
 
   async execute(interaction: CommandInteraction) {
     await interaction.deferReply();
+
     const pageSize =
       (interaction.options.get("page_size")?.value as number) || 5;
     let pageNumber = 1;
@@ -47,55 +52,72 @@ export class InRole extends BaseCommand {
     const embed = new EmbedBuilder()
       .setTitle(`Просмотр участников роли - ${role.name}`)
       .setColor(SnowflakeColors.DEFAULT)
-      .setFooter({
-        text: interaction.user.globalName,
-        iconURL: interaction.user.displayAvatarURL(),
-      })
       .setThumbnail(interaction.user.displayAvatarURL());
+
     const allMembers = (await interaction.guild.members.fetch()).filter(
       (member) => member.roles.cache.has(role.id)
     );
-    const disabledNextCondition = pageNumber * pageSize >= allMembers.size;
-    const disabledPreviousCodition = pageNumber === 1;
+    const lastPage = Math.ceil(allMembers.size / pageSize);
+    const updateButtons = () => {
+      const totalPages = Math.ceil(allMembers.size / pageSize);
+      const disabledNextCondition = pageNumber >= totalPages;
+      const disabledPreviousCondition = pageNumber <= 1;
+
+      firstRole.setDisabled(pageNumber === 1);
+      lastRole.setDisabled(pageNumber === totalPages);
+      next.setDisabled(disabledNextCondition);
+      previous.setDisabled(disabledPreviousCondition);
+    };
+
     const firstRole = new ButtonBuilder()
       .setCustomId(
         `firstRole_${interaction.user.id}_${interaction.guild.id}_${interaction.id}`
       )
       .setStyle(ButtonStyle.Secondary)
-      .setDisabled(disabledNextCondition)
       .setEmoji("⏮");
+
     const lastRole = new ButtonBuilder()
       .setCustomId(
         `lastRole_${interaction.user.id}_${interaction.guild.id}_${interaction.id}`
       )
       .setStyle(ButtonStyle.Secondary)
-      .setDisabled(disabledPreviousCodition)
       .setEmoji("⏭");
+
     const next = new ButtonBuilder()
       .setCustomId(`next_role_${interaction.user.id}`)
       .setEmoji("▶")
-      .setStyle(ButtonStyle.Secondary)
-      .setDisabled(disabledNextCondition);
+      .setStyle(ButtonStyle.Secondary);
+
     const previous = new ButtonBuilder()
       .setCustomId(`previousRole_${interaction.user.id}`)
       .setEmoji("◀")
-      .setStyle(ButtonStyle.Secondary)
-      .setDisabled(disabledPreviousCodition);
+      .setStyle(ButtonStyle.Secondary);
+
     const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
       firstRole,
       previous,
       next,
       lastRole
     );
-    const repl = interaction.editReply({
-      embeds: [
-        embed.setDescription(
-          this.setDescription(allMembers.clone().map((member) => member.id))
-        ),
-      ],
+
+    const updateEmbed = () => {
+      const arr = Array.from(allMembers.keys());
+      const start = (pageNumber - 1) * pageSize;
+      const end = start + pageSize;
+      const description = this.setDescription(arr.slice(start, end), start);
+      embed.setDescription(description);
+      embed.setFooter({ text: `Страница: ${pageNumber}/${lastPage}` });
+    };
+
+    updateEmbed();
+    updateButtons();
+
+    const repl = await interaction.editReply({
+      embeds: [embed],
       components: [row],
     });
-    const collector = (await repl).createMessageComponentCollector({
+
+    const collector = repl.createMessageComponentCollector({
       componentType: ComponentType.Button,
       time: 600_000,
       filter: (i) => i.user.id === interaction.user.id,
@@ -103,41 +125,34 @@ export class InRole extends BaseCommand {
 
     collector.on("collect", async (inter) => {
       await inter.deferUpdate();
-      if (inter.customId === "nuxtRole") {
-        pageNumber += 1;
-      } else if (inter.customId === "previousRole") {
+
+      if (inter.customId.includes("next_role")) {
+        pageNumber++;
+      } else if (inter.customId.includes("previousRole")) {
         pageNumber = Math.max(pageNumber - 1, 1);
       } else if (inter.customId.includes("firstRole")) {
         pageNumber = 1;
       } else if (inter.customId.includes("lastRole")) {
-        pageNumber = Math.ceil(allMembers.size / pageSize);
+        pageNumber = lastPage;
       }
-      const arr = allMembers.clone().map((member) => member.id);
-      const start = (pageNumber - 1) * pageSize;
-      const end = start + pageSize;
-      const description = this.setDescription(
-        Array.from(arr).slice(start, end)
-      );
-      inter.editReply({
-        embeds: [embed.setDescription(description)],
-        components: [
-          row.setComponents(
-            firstRole.setDisabled(disabledPreviousCodition),
-            previous.setDisabled(disabledPreviousCodition),
-            next.setDisabled(disabledNextCondition),
-            lastRole.setDisabled(disabledNextCondition)
-          ),
-        ],
+
+      updateEmbed();
+      updateButtons();
+
+      await inter.editReply({
+        embeds: [embed],
+        components: [row],
       });
     });
   }
 
-  private setDescription(members: string[]) {
-    let description = "";
-    members.map(
-      (member, index) =>
-        (description += `**${index + 1}.** ${userMention(member)}\n`)
-    );
-    return description;
+  private setDescription(members: string[], start: number) {
+    let start_ = start;
+    return members
+      .map((member) => {
+        start_ += 1;
+        return `**${start_}.** ${userMention(member)}`;
+      })
+      .join("\n");
   }
 }
