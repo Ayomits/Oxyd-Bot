015839946a94fc4d4992a33144ffb457e2da03ba
Ayomi <nikitakabo74@gmail.com>
2024-08-24 00:03:53 +0300

    fix: snowflake regex

--- src/utils/parsers/snowflakeParser.ts
+++ src/utils/parsers/snowflakeParser.ts
@@ -1,32 +1,29 @@
-// Parse any snowflake values
-// Such as role, user, channel etc.
-
 class SnowflakeParser {
   static parseAll(options: { content: string; parseMentions: boolean }) {
     const regex = options.parseMentions
-      ? /<(@|#|@&)(\d{17,19})?>/g
-      : /(\d{17,19})/g;
-    const find = options.content.match(regex);
-    return find;
+      ? /<(@|#|@&)(\d{17,19})>/g
+      : /\b(\d{17,19})\b/g;
+    const find = [...options.content.matchAll(regex)];
+    return find.map((match) => match[2] || match[1]);
   }
 
   static user(content: string) {
-    return this.baseParser(/<(@)(\d{17,19})/g, content);
+    return this.baseParser(/<@(\d{17,19})>/g, content);
   }
 
   static role(content: string) {
-    return this.baseParser(/<(@&)(\d{17,19})/g, content);
+    return this.baseParser(/<@&(\d{17,19})>/g, content);
   }
 
   static channel(content: string) {
-    return this.baseParser(/<(#)(\d{17,19})/g, content);
+    return this.baseParser(/<#(\d{17,19})>/g, content);
   }
 
   private static baseParser(regex: RegExp, content: string, isZero = false) {
-    const find = content.split(regex);
-    return find?.length <= 0 && !isZero
+    const find = [...content.matchAll(regex)];
+    return find.length <= 0 && !isZero
       ? this.baseParser(/(\d{17,19})/g, content, true)
-      : find;
+      : find.map((match) => match[1]);
   }
 }
 
