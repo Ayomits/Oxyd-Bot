73df11ef1c89e42d62b05aa240a34f71f6162e20
Ayomi <nikitakabo74@gmail.com>
2024-09-23 09:58:52 +0300

    chore: back old structure

--- src/db/models/economy/MaryModel.ts
+++ src/db/models/economy/MaryModel.ts
@@ -1,6 +1,6 @@
 import { model, Schema } from "mongoose";
 import { BaseGuildDocument } from "../../base/GuildDocument";
-import { MarryType } from "@/modules/entartaiment/marry-module/module/configs";
+import { MarryType } from "@/modules/marry-module/module/configs";
 
 export interface LoveRoomDocument {
   name: string;
