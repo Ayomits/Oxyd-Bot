5826c3942ecd95121aca643e2c8cbbb56926f6b5
Ayomi <nikitakabo74@gmail.com>
2024-10-06 23:16:22 +0300

    feat: teleport

---
+++ src/modules/teleport-module/module/VoiceStateUpdate.ts
@@ -0,0 +1,74 @@
+import BaseEvent from "@/abstractions/BaseEvent";
+import { TeleportModel } from "@/db/models/teleport/TeleportModel";
+import { TeleportSettingsModel } from "@/db/models/teleport/TeleportSettingsModel";
+import { randomValue } from "@/libs/embeds-functions/random";
+import { ChannelType, Events, VoiceChannel, VoiceState } from "discord.js";
+
+export class TeleportVoiceStateUpdate extends BaseEvent {
+  constructor() {
+    super({
+      name: Events.VoiceStateUpdate,
+      once: false,
+    });
+  }
+
+  async execute(_oldState: VoiceState, newState: VoiceState) {
+    if (!newState) return;
+    const member = newState.member;
+    const channel = newState.channel;
+
+    const [teleportSettings, teleport] = await Promise.all([
+      TeleportSettingsModel.findOne({ guildId: newState.guild.id }),
+      TeleportModel.findOne({
+        guildId: newState.guild.id,
+        channelId: channel?.id,
+      }),
+    ]);
+    if (!teleportSettings || !teleport) {
+      return;
+    }
+    if (!teleportSettings.enable || !teleport.enable) {
+      return;
+    }
+    const values = [
+      teleport.categories.filter((channel) =>
+        newState.guild.channels.cache.get(channel)
+      ),
+      teleport.channels.filter((channel) => {
+        const isIgnored = !teleport.ignoredChannels.includes(channel);
+        const channelFromGuild = newState.guild.channels.cache.get(channel);
+        if (isIgnored) return false;
+        if (!channelFromGuild) return false;
+        console.log(channelFromGuild);
+        if (!channelFromGuild.isVoiceBased()) return false;
+        return true;
+      }),
+    ].filter((arr) => {
+      if (!arr || arr.length <= 0) return false;
+      return true;
+    });
+    const randomArr = randomValue(values);
+    const randomArrValue = randomValue(randomArr);
+    const randomArrChannel = newState.guild.channels.cache.get(randomArrValue);
+
+    if (randomArrChannel.type === ChannelType.GuildCategory) {
+      let randomChannel;
+      const channels = newState.guild.channels.cache.filter(
+        (channel) => channel.type === ChannelType.GuildVoice &&
+        channel.parentId === randomArrChannel.id &&
+          !teleport.ignoredChannels.includes(channel.id)
+      );
+      if (channels.size <= 0) {
+        if (values.length >= 2) {
+          const arr = values[1];
+          if (arr.length <= 0) return;
+          randomChannel = newState.guild.channels.cache.get(randomValue(arr));
+        }
+      }
+      randomChannel = channels.random();
+      return await member.voice.setChannel(randomChannel);
+    } else {
+      member.voice.setChannel(randomArrChannel as VoiceChannel);
+    }
+  }
+}
